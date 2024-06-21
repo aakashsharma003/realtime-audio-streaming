9cf02833c8ddfe429cb6dbe0221f3aa01a29d777
aakashsharma003 <aakash6263264@gmail.com>
2024-06-21 12:00:51 +0530

    adding more styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+
       <HeroSection />
+      </div>
     </main>
   );
 }
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,40 +1,68 @@
-import React from "react";
 import Link from "next/link";
-import { Button } from "@/components/ui/moving-border";
 import { Spotlight } from "./ui/Spotlight";
-function HeroSection() {
-  return (
-    <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
-      <Spotlight
-        className="-top-40 left-0 md:left-60 md:-top-20"
-        fill="white"
-      />
+import { TextGenerateEffect } from "./ui/text-generate-effect";
+import { Button } from "./ui/moving-border";
 
-      <div className="p-4 relative z-10 w-full text-center">
-        <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
-          Realtime audio Streaming library
-        </h1>
-        <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
-          Welcome to the Music Library API, your ultimate gateway to a world of
-          music. Inspired by top-tier music streaming services like Spotify,
-          this API delivers a seamless and immersive experience, allowing you to
-          integrate rich music functionalities into your applications. Whether
-          you are building the next big music app or simply want to add some
-          rhythm to your project, our API has you covered.
-        </p>
-        <div className="mt-4">
-          <Link href={"/documentation"}>
-            <Button
-              borderRadius="1.75rem"
-              className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
-            >
-              Getting Started
-            </Button>
-          </Link>
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-36">
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="h-[80vh] w-[50vw] top-10 left-full"
+          fill="purple"
+        />
+        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+
+      <div
+        className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
+       absolute top-0 left-0 flex items-center justify-center"
+      >
+        <div
+          className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
+         bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+        />
+      </div>
+
+      <div className="flex justify-center relative my-20 z-10">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          {/* todo */}
+          {/* <TextGenerateEffect
+          className="text-center text-[340px]"
+            words="RealTime audio Streaming library"
+          /> */}
+
+          <h1 className="whitespace-nowrap my-10 py-2 md:mt-0 text-lg md:text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
+            Realtime audio Streaming library
+          </h1>
+
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl my-2">
+            Hi, get ready to Dive in and explore a world of endless musical
+            possibilities, crafted just for you.
+          </p>
+
+          <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            Dynamic Web Magic with Next.js
+          </p>
+
+          <div className="mt-4">
+            <Link href={"/documentation"}>
+              <Button
+                borderRadius="1.75rem"
+                className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
+              >
+                Getting Started
+              </Button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default HeroSection;
+export default Hero;
